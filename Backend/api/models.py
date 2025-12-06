124439afb400d7fcd069ba1cf3d9370ee3fc73be
qqmeg <michail.shirshov@gmail.com>
2025-12-06 23:51:06 +0300

    Changed frontend form a lil bit and added a lot of debug quality-of-life things

--- Backend/api/models.py
+++ Backend/api/models.py
@@ -4,9 +4,9 @@ from django.db import models
 class User(models.Model):
     id = models.AutoField(primary_key=True)
     email = models.EmailField(unique=True)
-    username = models.TextField(verbose_name="Имя", max_length=30, blank="user")
+    username = models.TextField(verbose_name="Имя", max_length=30, blank=True)
     # avatar = models.ImageField(verbose_name='Аватарка', upload_to='avatars/', null=True, blank=True)
-    bio = models.TextField(verbose_name="О себе", null=True, blank="no bio yet")
+    bio = models.TextField(verbose_name="О себе", null=True, default="no bio yet")
     passw = models.TextField(verbose_name="Пароль", max_length=100, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
--- Backend/api/views.py
+++ Backend/api/views.py
@@ -1,26 +1,43 @@
-
-from rest_framework.decorators import api_view
-
-from rest_framework.response import Response
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
 from .models import User
 from .serializers import UserSerializer
 
 
-@api_view(['GET'])
+@csrf_exempt
+@api_view(["GET"])
 def getuser(request, user_id=None):
     try:
-        
         user = User.objects.get(id=user_id)
         serializer = UserSerializer(user)
         return Response(serializer.data)
     except User.DoesNotExist:
-        return Response({'error': 'User not found'}, status=status.HTTP_404_NOT_FOUND)
+        return Response({"error": "User not found"}, status=status.HTTP_404_NOT_FOUND)
 
 
-@api_view(['POST'])
+@csrf_exempt
+@api_view(["POST"])
 def create_user(request):
-    serializer = UserSerializer(data=request.data)
+    data = request.data
+    serializer = UserSerializer(data=data)
+    if not serializer.is_valid():
+        print(serializer.errors)
+    if not data.get("email"):
+        return JsonResponse({"error": "Email обязателен"}, status=400)
+    if not data.get("passw"):
+        return JsonResponse({"error": "Пароль обязателен"}, status=400)
+    if not data.get("username"):
+        return JsonResponse({"error": "Имя пользователя обязательно"}, status=400)
+
+    if User.objects.filter(email=data["email"]).exists():
+        return JsonResponse(
+            {"error": "Пользователь с таким email уже существует"}, status=400
+        )
+
     if serializer.is_valid():
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
