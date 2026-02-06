85fdb242c8bb505adaabec57ad0f305ae32cc7c9
qqmeg <michail.shirshov@gmail.com>
2026-02-06 11:10:24 +0300

    base for urls handling is kinda done

--- Backend/api/serializers.py
+++ Backend/api/serializers.py
@@ -6,3 +6,28 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = '__all__'
+
+
+class UserPublicSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ("id", "email", "username", "bio", "created_at")  #только поля которые нельзя изменять
+        read_only_fields = fields
+
+
+class UserMeUpdateSerializer(serializers.ModelSerializer): #обновление профиля юзера
+    class Meta:
+        model = User
+        fields = ("username", "bio")
+
+
+class RegisterSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+    passw = serializers.CharField()
+    username = serializers.CharField(required=False, allow_blank=True)
+    bio = serializers.CharField(required=False, allow_blank=True, allow_null=True)
+
+
+class LoginSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+    passw = serializers.CharField()
--- Backend/api/urls.py
+++ Backend/api/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from .views import getuser, create_user
+from .views import getuser, create_user, auth_register, auth_login, me
 
 
 urlpatterns = [
     path("users/<int:user_id>/", getuser, name='getuser'),
     path("users/create/", create_user, name='createuser'),
+    path("auth/register/", auth_register, name="auth_register"),
+    path("auth/login/", auth_login, name="auth_login"),
+    path("me/", me, name="me"),
 ]
